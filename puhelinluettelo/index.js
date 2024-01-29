217e6dbc05c9c6d0cbe9581339144402931c3488
AarnoVeitola <121627348+AarnoVeitola@users.noreply.github.com>
2024-01-29 12:35:16 +0200

    added frontend functionality for exercise 3.9

--- puhelinluettelo/index.js
+++ puhelinluettelo/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', (req, res) => {
     return JSON.stringify(req.body)
 })
 
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -123,7 +126,7 @@ app.get('/info', (request, response) => {
     `)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
